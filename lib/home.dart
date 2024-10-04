197e412395443424460295490bccab09ad4d7ce4
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-04 20:09:45 +0300

    check user permission to send notifications

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,30 @@
+import 'dart:developer';
+
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    AwesomeNotifications().isNotificationAllowed().then(
+      (value) {
+        if (value) {
+          log('Allowed');
+        } else {
+          log('Not Allowed');
+          AwesomeNotifications().requestPermissionToSendNotifications();
+        }
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
