d834555e9da01aadd44e3f077fa3b38f57653973
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-05 01:53:56 +0300

    added firebase cloud messaging with Awesome Notification package

--- lib/home.dart
+++ lib/home.dart
@@ -17,8 +17,21 @@ class _HomeState extends State<Home> {
   void initState() {
     FirebaseMessaging.onMessage.listen(
       (event) {
-        log(event.notification!.title.toString());
-        log(event.notification!.body.toString());
+        AwesomeNotifications().createNotification(
+          content: NotificationContent(
+            id: 1,
+            channelKey: 'basic',
+            title: event.notification!.title,
+            body: event.notification!.body,
+            duration: const Duration(seconds: 2),
+            actionType: ActionType.Default,
+            roundedBigPicture: true,
+            displayOnBackground: true,
+            fullScreenIntent: true,
+            notificationLayout: NotificationLayout.BigPicture,
+            wakeUpScreen: true,
+          ),
+        );
       },
     );
     AwesomeNotifications().isNotificationAllowed().then(
@@ -50,35 +63,7 @@ class _HomeState extends State<Home> {
       body: SafeArea(
         child: Center(
           child: ElevatedButton(
-            onPressed: () async {
-              final fcmToken = await FirebaseMessaging.instance.getToken();
-              log(fcmToken.toString());
-              await AwesomeNotifications()
-                  .createNotification(
-                content: NotificationContent(
-                  id: 1,
-                  channelKey: 'basic',
-                  title: 'Hi',
-                  body: 'I Sent A Notification',
-                  duration: const Duration(seconds: 2),
-                  actionType: ActionType.Default,
-                  roundedBigPicture: true,
-                  displayOnBackground: true,
-                  fullScreenIntent: true,
-                  notificationLayout: NotificationLayout.BigPicture,
-                  wakeUpScreen: true,
-                ),
-              )
-                  .then(
-                (value) {
-                  if (value) {
-                    log('Sent');
-                  } else {
-                    log('Failed');
-                  }
-                },
-              );
-            },
+            onPressed: () async {},
             child: const Text('Send'),
           ),
         ),
