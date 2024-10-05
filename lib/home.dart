b662821695051d2928cc51ef0df5a02d626b81cc
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-05 17:23:34 +0300

    used the new class

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,8 @@
 import 'dart:developer';
-
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:test/notification_controller.dart';
 
 @pragma("vm:entry-point")
 class Home extends StatefulWidget {
@@ -15,44 +15,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   void initState() {
-    AwesomeNotifications().isNotificationAllowed().then(
-      (value) {
-        if (value) {
-          FirebaseMessaging.onMessage.listen(
-            (event) {
-              AwesomeNotifications().createNotification(
-                content: NotificationContent(
-                  id: 1,
-                  channelKey: 'basic',
-                  title: event.notification!.title,
-                  body: event.notification!.body,
-                  duration: const Duration(seconds: 2),
-                  actionType: ActionType.Default,
-                  roundedBigPicture: true,
-                  displayOnBackground: true,
-                  fullScreenIntent: true,
-                  notificationLayout: NotificationLayout.BigPicture,
-                  wakeUpScreen: true,
-                ),
-              );
-            },
-          );
-        } else {
-          log('Not Allowed');
-          AwesomeNotifications()
-              .requestPermissionToSendNotifications(channelKey: 'basic');
-        }
-      },
-    );
-
-    AwesomeNotifications().setListeners(
-      onActionReceivedMethod: (receivedAction) async {
-        log(receivedAction.body!);
-      },
-      onNotificationDisplayedMethod: (receivedNotification) async {
-        log(receivedNotification.body!);
-      },
-    );
+    NotificationController.notificationController.requestUserPermission();
+    NotificationController.notificationController.setPackageListeners();
     super.initState();
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,10 @@
-import 'package:awesome_notifications/awesome_notifications.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:test/firebase_options.dart';
 import 'package:test/home.dart';
+import 'package:test/notification_controller.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  AwesomeNotifications().initialize(
-    null,
-    [
-      NotificationChannel(
-        channelKey: 'basic',
-        channelName: 'basic_channel',
-        channelDescription: 'Notification Channel',
-        defaultRingtoneType: DefaultRingtoneType.Notification,
-        playSound: true,
-        criticalAlerts: true,
-        enableVibration: true,
-        importance: NotificationImportance.Max,
-        channelShowBadge: true,
-        enableLights: true,
-      ),
-    ],
-    debug: true,
-  );
-  await FirebaseMessaging.instance.setAutoInitEnabled(true);
-
+  NotificationController.notificationController.initFirebaseCloudMessaging();
+  NotificationController.notificationController.initNotificationPackage();
   runApp(const Test());
 }
 
