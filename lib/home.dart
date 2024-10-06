d3651c2a76585b8fc17d404547030f3daf294b4a
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-06 19:06:07 +0300

    created paymob auth token

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test/paymob_payment.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
--- lib/paymob_payment.dart
+++ lib/paymob_payment.dart
@@ -2,4 +2,19 @@ import 'package:dio/dio.dart';
 
 class PaymobPayment {
   final dio = Dio();
+
+  authenticate() async {
+    var response = await dio.post(
+      'https://accept.paymob.com/api/auth/tokens',
+      data: {
+        "username": "01000512414",
+        "password": "1172003Ahmed#",
+      },
+      options: Options(
+        contentType: 'application/json',
+      ),
+    );
+
+    return response.data['token'];
+  }
 }
