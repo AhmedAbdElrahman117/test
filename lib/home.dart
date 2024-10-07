8021e89ec2841cacab9f16c8e6eb4c3afe4c86f2
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-07 18:47:24 +0300

    DraggableScrollableSheet full example

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,34 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.greenAccent,
+          ),
+          DraggableScrollableSheet(
+            initialChildSize: 0.1,
+            minChildSize: 0.1,
+            maxChildSize: 1,
+            snap: true,
+            builder: (context, scrollController) {
+              return CustomScrollView(
+                controller: scrollController,
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: Container(
+                      color: Colors.black,
+                      width: 500,
+                      height: MediaQuery.sizeOf(context).height,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
