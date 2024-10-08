935f8e507c970727171c82b7e97fbfd07eb5a438
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-08 17:25:06 +0300

    animated align example

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,49 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool animate = false;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedAlign(
+                alignment:
+                    animate ? Alignment.centerRight : Alignment.centerLeft,
+                duration: Duration(milliseconds: 500),
+                child: Text('data'),
+              ),
+              SizedBox(height: 60),
+              ElevatedButton(
+                onPressed: () {
+                  animate = !animate;
+                  setState(() {});
+                  Timer(
+                    Duration(milliseconds: 600),
+                    () {
+                      animate = !animate;
+                      setState(() {});
+                    },
+                  );
+                },
+                child: Text('Animate'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
