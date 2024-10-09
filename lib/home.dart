b860566716fba677c9e047d666017671b98bb374
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-09 17:36:58 +0300

    installed auto_animated package

--- lib/home.dart
+++ lib/home.dart
@@ -1,63 +1,14 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  ItemScrollController scrollController = ItemScrollController();
-  ItemPositionsListener? positionsListener;
-  int current = 0;
-  bool isCurrent = false;
-
-  @override
-  void initState() {
-    positionsListener = ItemPositionsListener.create()
-      ..itemPositions.addListener(
-        () {},
-      );
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          current = 17;
-          scrollController.scrollTo(
-            index: current,
-            duration: const Duration(milliseconds: 200),
-          );
-          isCurrent = !isCurrent;
-          setState(() {});
-          Timer(
-            const Duration(milliseconds: 500),
-            () {
-              isCurrent = !isCurrent;
-              setState(() {});
-            },
-          );
-        },
-      ),
       body: SafeArea(
-        child: ScrollablePositionedList.builder(
-          itemCount: 20,
-          itemScrollController: scrollController,
-          itemPositionsListener: positionsListener,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('data${index + 1}'),
-              selected: current == index ? isCurrent : false,
-              selectedTileColor: Colors.grey,
-            );
-          },
+        child: Column(
+          children: [],
         ),
       ),
     );
