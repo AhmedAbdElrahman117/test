ca77d064f3b3a65d87a03aad01ba9156498af086
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-09 15:29:32 +0300

    slidable package example

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Slidable(
+              endActionPane: ActionPane(
+                motion: const ScrollMotion(),
+                children: [
+                  const SizedBox(width: 10),
+                  SlidableAction(
+                    onPressed: (context) {},
+                    label: 'Share',
+                    borderRadius: BorderRadius.circular(20),
+                    backgroundColor: Colors.blue,
+                    autoClose: false,
+                    icon: Icons.share,
+                  ),
+                  const SizedBox(width: 10),
+                  SlidableAction(
+                    onPressed: (context) {},
+                    label: 'Share',
+                    borderRadius: BorderRadius.circular(20),
+                    backgroundColor: Colors.blue,
+                    autoClose: false,
+                    icon: Icons.share,
+                  ),
+                  const SizedBox(width: 10),
+                ],
+              ),
+              child: const ListTile(
+                title: Text('data'),
+                tileColor: Colors.amber,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
