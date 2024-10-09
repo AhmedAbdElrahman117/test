46d0743008dc97e23b9f1f85e9f69b3645611ce8
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-09 18:33:38 +0300

    Skeletonizer example

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,43 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Home extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool enable = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        enable = !enable;
+        setState(() {});
+      }),
       body: SafeArea(
-        child: Column(
-          children: [],
+        child: ListView.builder(
+          itemCount: 15,
+          itemBuilder: (context, index) {
+            return Skeletonizer(
+              enabled: enable,
+              ignoreContainers: true,
+              ignorePointers: true,
+              effect: ShimmerEffect(),
+              enableSwitchAnimation: true,
+              child: Card(
+                child: ListTile(
+                  title: Text('data'),
+                  subtitle: Text('data'),
+                  trailing: Icon(Icons.abc),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
