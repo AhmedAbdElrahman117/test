39fad986509ec1b0089e33576b334a69b5317def
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-09 18:19:29 +0300

    added skeletonizer package

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,4 @@
-import 'package:auto_animated/auto_animated.dart';
 import 'package:flutter/material.dart';
-import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -9,27 +7,8 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: AnimateIfVisibleWrapper(
-          showItemInterval: Duration(milliseconds: 500),
-          child: ScrollablePositionedList.builder(
-            itemCount: 5,
-            itemBuilder: (context, index) {
-              return AnimateIfVisible(
-                reAnimateOnVisibility: true,
-                key: Key('k$index'),
-                builder: (context, animation) {
-                  return FadeTransition(
-                    opacity: animation,
-                    child: Container(
-                      width: MediaQuery.sizeOf(context).width,
-                      height: MediaQuery.sizeOf(context).height,
-                      color: Colors.accents[index],
-                    ),
-                  );
-                },
-              );
-            },
-          ),
+        child: Column(
+          children: [],
         ),
       ),
     );
