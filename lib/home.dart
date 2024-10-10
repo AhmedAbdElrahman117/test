df9e3689e5b10a217c53fcb6f452cb64911c3735
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-10 16:57:14 +0300

    setting overlay example

--- lib/home.dart
+++ lib/home.dart
@@ -1,43 +1,17 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:skeletonizer/skeletonizer.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  bool enable = false;
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        enable = !enable;
-        setState(() {});
-      }),
       body: SafeArea(
-        child: ListView.builder(
-          itemCount: 15,
-          itemBuilder: (context, index) {
-            return Skeletonizer(
-              enabled: enable,
-              ignoreContainers: true,
-              ignorePointers: true,
-              effect: ShimmerEffect(),
-              enableSwitchAnimation: true,
-              child: Card(
-                child: ListTile(
-                  title: Text('data'),
-                  subtitle: Text('data'),
-                  trailing: Icon(Icons.abc),
-                ),
-              ),
-            );
-          },
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('Show Overlay'),
+          ),
         ),
       ),
     );
