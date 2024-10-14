5ee9e8ae8410e95f87938617f5978337c9f85992
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-14 21:12:33 +0300

    overlay portal example

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,47 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    OverlayPortalController controller = OverlayPortalController();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: TextButton(
+                onPressed: () => controller.toggle(),
+                child: OverlayPortal(
+                  controller: controller,
+                  overlayChildBuilder: (context) {
+                    return Stack(
+                      alignment: Alignment.lerp(
+                          Alignment.center, Alignment.bottomCenter, 0.2)!,
+                      children: [
+                        Container(
+                          width: 200,
+                          height: 40,
+                          color: Colors.grey,
+                          child: const Text(
+                            'This is Overlay Portal',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                  child: const Text(
+                    'data',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
