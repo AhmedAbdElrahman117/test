77cdb3c054546130981a8fa1f3b233c74c635687
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-16 23:05:47 +0300

    done

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,53 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: FocusScope(
+          autofocus: true,
+          onFocusChange: (value) {},
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Focus(
+                child: TextField(
+                  onSubmitted: (value) {},
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              Focus(
+                child: TextField(
+                  onSubmitted: (value) {},
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              Focus(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
