73154afc64c47247aad05fe466464f4573e74ab2
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-16 22:25:38 +0300

    ScrollNotification example

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,49 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: NotificationListener<ScrollNotification>(
+          onNotification: (notification) {
+            if (notification is ScrollStartNotification) {
+              log('Start');
+            } else if (notification is ScrollEndNotification) {
+              setState(() {
+                log('End');
+              });
+            } else if (notification is ScrollUpdateNotification) {
+              log('Moving');
+            }
+            return true;
+          },
+          child: ListView.builder(
+            itemCount: 50,
+            itemBuilder: (context, index) {
+              return ListTile(
+                key: Key('W$index'),
+                title: Text(
+                  'Data${index + 1}',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
