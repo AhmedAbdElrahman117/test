1b291a3367f1a83a37205dcfbcff0f57409fe3fd
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-17 10:40:17 +0300

    flutter_floating_bottom_bar example

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:flutter_floating_bottom_bar/flutter_floating_bottom_bar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,39 +9,82 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  bool enabled = false;
-  @override
-  void initState() {
-    getPermission();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: FutureBuilder(
-        future: FlutterContacts.getContacts(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Text('Error');
-          } else if (!snapshot.hasData) {
-            return Text('No Data');
-          } else {
-            return ListTile(
-              title: Text(snapshot.data!.first.displayName),
-              subtitle: Text(snapshot.data!.first.name.firstPhonetic),
-            );
-          }
-        },
-      )),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        body: SafeArea(
+          child: BottomBar(
+            clip: Clip.none,
+            icon: (width, height) {
+              return Center(
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.arrow_upward,
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+            showIcon: true,
+            iconHeight: 40,
+            iconWidth: 40,
+            borderRadius: BorderRadius.circular(50),
+            width: MediaQuery.sizeOf(context).width * 0.8,
+            body: (context, controller) => TabBarView(
+              children: [
+                ListView.builder(
+                  itemCount: 100,
+                  controller: controller,
+                  itemBuilder: (context, index) {
+                    return const Text('data');
+                  },
+                ),
+                Container(color: Colors.amberAccent),
+                Container(color: Colors.green),
+                Container(color: Colors.yellowAccent),
+              ],
+            ),
+            child: Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: [
+                const TabBar(
+                  tabAlignment: TabAlignment.fill,
+                  tabs: [
+                    SizedBox(
+                      width: 40,
+                      height: 55,
+                      child: Center(child: Icon(Icons.home)),
+                    ),
+                    SizedBox(
+                      width: 40,
+                      height: 55,
+                      child: Center(child: Icon(Icons.home)),
+                    ),
+                    SizedBox(
+                      width: 40,
+                      height: 55,
+                      child: Center(child: Icon(Icons.home)),
+                    ),
+                    SizedBox(
+                      width: 40,
+                      height: 55,
+                      child: Center(child: Icon(Icons.home)),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  top: -40,
+                  child: FloatingActionButton(onPressed: () {}),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
-  }
-
-  getPermission() async {
-    if (await FlutterContacts.requestPermission()) {
-      enabled = true;
-      setState(() {});
-    }
   }
 }
