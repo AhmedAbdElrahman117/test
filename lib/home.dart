1cc1f245f9e7b112d6b9df291e2dafafe4fae65a
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-17 15:51:42 +0300

    added clay container package

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,4 @@
-import 'package:fade_out_particle/fade_out_particle.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,32 +8,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  bool animated = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FadeOutParticle(
-              disappear: animated,
-              duration: const Duration(milliseconds: 2000),
-              child: const Center(
-                child: Text(
-                  'Hello Ahmed',
-                  style: TextStyle(fontSize: 30),
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                animated = !animated;
-                setState(() {});
-              },
-              child: const Text('Animate'),
-            ),
-          ],
+          children: [],
         ),
       ),
     );
