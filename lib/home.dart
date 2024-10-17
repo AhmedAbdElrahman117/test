50a420c6a8ec098b1b44dc36accb77460196706c
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-17 15:40:14 +0300

    added fade_out_particle package

--- lib/home.dart
+++ lib/home.dart
@@ -9,22 +9,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  ScrollController controller1 = ScrollController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          children: [
-            QrImageView(
-              data: 'Hello World',
-              backgroundColor: Colors.amber,
-              size: 400,
-              version: QrVersions.auto,
-              embeddedImage: NetworkImage(
-                  'https://www.shutterstock.com/image-vector/letter-qr-logo-design-vector-260nw-1445885003.jpg'),
-            ),
-          ],
+          children: [],
         ),
       ),
     );
