0d86b9db30fe96ef0ee16c949fc0e633036d4a07
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-17 09:15:00 +0300

    stateful Widget

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
