d1f86d05a64629501619c6901c43b6be0e3dbc19
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-17 09:51:16 +0300

    created Search delegate class

--- lib/delegate.dart
+++ lib/delegate.dart
@@ -1,27 +1,61 @@
 import 'package:flutter/material.dart';
 
-class Delegate extends SearchDelegate<dynamic> {
+class Delegate extends SearchDelegate<String> {
+  List<String> data;
+  Delegate(this.data);
   @override
   List<Widget>? buildActions(BuildContext context) {
-    // TODO: implement buildActions
-    throw UnimplementedError();
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear_rounded),
+      ),
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
-    throw UnimplementedError();
+    return const BackButton();
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
-    throw UnimplementedError();
+    return ListView(
+      children: data.map(
+        (e) {
+          if (e.contains(query)) {
+            return ListTile(
+              title: Text(e),
+            );
+          } else {
+            return Container();
+          }
+        },
+      ).toList(),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    throw UnimplementedError();
+    return ListView(
+      children: data.map(
+        (e) {
+          if (e.contains(query)) {
+            return ListTile(
+              title: Text(e),
+            );
+          } else {
+            return Container();
+          }
+        },
+      ).toList(),
+    );
+  }
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    return Theme.of(context);
   }
 }
