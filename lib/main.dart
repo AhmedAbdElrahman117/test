98353a8f456f8e42e2afbd4dc7b0422d25b8eff0
Ahmed  Abo Elnaga <ahmedaboelnaga713@gmail.com>
2024-10-18 06:12:12 +0300

    splash view example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:splash_view/source/presentation/pages/pages.dart';
+import 'package:splash_view/source/presentation/presentation.dart';
 import 'package:test/home.dart';
 
 void main() {
@@ -10,9 +12,44 @@ class Test extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: SplashView(
+        // backgroundColor: Colors.black,
+        duration: const Duration(seconds: 20),
+        logo: Container(color: Colors.amber, width: 200, height: 200),
+        title: const Text(
+          'UCCD',
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+          ),
+        ),
+        subtitle: const Text(
+          'BTU',
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+          ),
+        ),
+        gradient: const LinearGradient(
+          colors: [
+            Colors.amberAccent,
+            Colors.greenAccent,
+          ],
+        ),
+        loadingIndicator: const Padding(
+          padding: EdgeInsets.fromLTRB(0, 12, 0, 80),
+          child: CircularProgressIndicator.adaptive(),
+        ),
+        bottomLoading: true,
+        showStatusBar: true,
+        done: Done(
+          const Home(),
+          animationDuration: const Duration(milliseconds: 500),
+          curve: Curves.easeInOutBack,
+        ),
+      ),
     );
   }
 }
